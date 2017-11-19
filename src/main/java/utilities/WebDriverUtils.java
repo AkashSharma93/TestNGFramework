b3145910fc9b36fdc1d0a4994109877d08b8fbc4
Akash Sharma S <present.akash@gmail.com>
2017-11-20 00:09:20 +0530

    Added java source/target level in pom. Also, added web driver util methods - waitForElement, invokeAgain, and more helper methods for that.

--- src/main/java/utilities/WebDriverUtils.java
+++ src/main/java/utilities/WebDriverUtils.java
@@ -1,7 +1,80 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 /**
  * Created by akash on 19/11/17.
  */
 public class WebDriverUtils {
+    WebDriver webDriver;
+
+    public WebDriverUtils(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public WebDriver getWebDriver() {
+        return webDriver;
+    }
+
+    private boolean invokeAgainInCaseOfFailure(Runnable action) throws Exception {
+        return invokeAgainInCaseOfFailure(action, 0);
+    }
+
+    private boolean invokeAgainInCaseOfFailure(Runnable action, int recursionLevel) throws Exception {
+        try {
+            action.run();
+        } catch (Exception e) {
+            if (recursionLevel > 0) {   // Fail
+                throw e;
+            } else {    // Try again
+                return invokeAgainInCaseOfFailure(action, recursionLevel + 1);
+            }
+        }
+        return true;
+    }
+
+    public void waitForPageLoad() {
+        WebDriverWait wait = new WebDriverWait(webDriver, 20, 500);
+        wait.until((WebDriver webDriver) -> {
+            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+            return javascriptExecutor.executeScript("return document.readyState").equals("complete");
+        });
+
+    }
+
+    public ExpectedCondition<WebElement> isElementVisible(String xpath) {
+        waitForPageLoad();
+
+        return (WebDriver webDriver) -> {
+            WebElement webElement = webDriver.findElement(By.xpath(xpath));
+            if (webElement != null && webElement.isEnabled() && webElement.isDisplayed()) {
+                return webElement;
+            }
+            return null;
+        };
+    }
+
+    public boolean waitForElement(String xpath, int waitTime) {
+        System.out.println(String.format("Waiting for - xpath: %s  Wait-time: %d", xpath, waitTime));
+        try {
+            return invokeAgainInCaseOfFailure(getWaitForElementAction(xpath, waitTime));
+        } catch (Exception e) {
+            // Masking this exception, as waitForElement can be used for negative scenarios too.
+        }
+        return false;
+    }
+
+    // Returning a runnable, as I may want to use this action outside the scope of invokeAgain method.
+    public Runnable getWaitForElementAction(String xpath, int waitTime) {
+        return () -> {
+            Wait<WebDriver> wait = new WebDriverWait(webDriver, waitTime);
+            wait.until(isElementVisible(xpath));
+        };
+    }
 }
